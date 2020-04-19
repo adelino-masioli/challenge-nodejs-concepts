71a66a9b7339fe924f22292541ae53f4649baaac
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-04-19 14:17:10 +0100

    NodeJS Concepts Challenge

---
+++ README.md
@@ -0,0 +1,16 @@
+<img alt="GoStack" src="src/assets/go-stack.png" />
+
+<h3 align="center">
+  NodeJS Concepts Challenge
+</h3>
+
+## :rocket: About the challenge
+
+During this project the concepts were applied with the implementation of a simple CRUD using NodeJS, Express with return in JSON in order to serve the front-end in ReactJS and the Mobile application in ReactJS.
+
+The tests were properly run and passed, so it was respected and followed as requested by the task.
+
+Below is a preview of how the application looks.
+
+<img alt="GoStack" src="src/assets/nodejs-visual-studio-code.png" />
+<img alt="GoStack" src="src/assets/nodejs-insommia.png" />
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,87 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function validadeRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if (!IsUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository ID.' });
+  }
+
+  return next();
+}
+
+app.use('/respositories/:id', validadeRepositoryId);
+
+app.get('/repositories', (request, response) => {
+  const result = repositories;
+
+  return response.json(result);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { url, title, techs } = request.body;
+  const repository = { id: uuid(), url, title, techs, likes: 0 };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  if (request.body.likes) {
+    return response.status(403).json(repositories[repositoryIndex]);
+  }
+
+  const repository = {
+    id,
+    url: url,
+    title: title,
+    techs: techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  return response.status(200).json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
